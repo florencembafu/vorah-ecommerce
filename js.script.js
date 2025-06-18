39c15f6c2b459ccaea25d58178c7036a6a778438
florencembafu <mbafumoyafl@icloud.com>
2025-06-18 09:55:49 +0200

    commit4

--- js.script.js
+++ js.script.js
@@ -1,3 +1,17 @@
 function ajouterPanier( ) {
     alert("produit ajouté au panier!");
 }
+
+function filterProducts(category) {
+  const allProducts=document.querySelectorAll('.product-card');
+  allProducts.forEach(product => {
+        if (product.classList.contains(category)){
+            product.Style.display = "block";
+        }   else{
+            product.Style.display = "none";
+        }
+    });
+}
+window.onload = function(){
+    filterProducts('offres spéciale');
+};
